0997b3d4d61e0beb3ce47249730831bf39ffd0b9
JORGE CIFUENTES <jocifuentesguz@gmail.com>
2024-04-27 19:07:13 -0500

    Temas tercera clase

---
+++ TerceraClase/0_Archivo_Uno.py
@@ -0,0 +1,29 @@
+
+#print() permite mostrar en la terminal la información
+print("Hola Mundo")
+
+#Esta es una variable
+a = type(8)
+variable = 7 
+
+#Al correr el programa arroja "int" para números enteros; "float" para decimales y "str" para texto, str significa string o cadena en inglés
+"""
+print(a) 
+print(type(1.8)) 
+print(type("texto")) 
+"""
+
+#CtrolK + ControlC para agregar comentarios
+
+#Controlk + ControlU para des hacer comentarios
+
+#Si no se puede hacer mediante comandos en el teclado se puede hacer con tres comillas dobles arriba y tres comillas dobles abajo
+#La sintatxis correcta para nombrar las variables es en minúscula y con guiónes abajo para nombrar su respectivo consecutivo, si se trabaja en una empresa la recomendación es que se escriban las variables directamente en inglés
+
+variabale_dos = "Texto dos"
+my_string = "thisStringisAwesome"
+#el Comando  .upper permite cambiar los textos a todas mayúsculas
+
+print(variabale_dos)
+print(my_string.upper())
+
